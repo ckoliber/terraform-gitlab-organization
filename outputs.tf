ec419c4b21fc104d5c02a08739771db08d48d05b
KoLiBer <koliberr136a1@gmail.com>
2022-11-05 06:58:24 +0000

    feat: add application group id to output values

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "id" {
+  value       = gitlab_group.parent.id
+  sensitive   = false
+  description = "Application ID"
+}
+
 output "repositories" {
   value       = { for key, val in var.repositories : key => gitlab_project.this[key].id }
   sensitive   = false
